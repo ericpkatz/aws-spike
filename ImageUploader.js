fd72398ff2b166110714431fb90363040f12b7fb
Eric P. Katz <ericpkatz@gmail.com>
2019-05-23 17:48:59 -0400

    commit

--- ImageUploader.js
+++ ImageUploader.js
@@ -5,16 +5,15 @@ const credentials = {
   AWS_ACCESS_KEY_ID: config.get('AWS_ACCESS_KEY_ID'),
   AWS_SECRET_ACCESS_KEY: config.get('AWS_SECRET_ACCESS_KEY'),
 };
-console.log(credentials);
 const S3 = new AWS.S3(credentials);
 
 const upload = (data)=> {
   const regex = new RegExp(/^data:image\/(\w+);.*/);
   const extension = regex.exec(data)[1];
-  const buffer = new Buffer(data.replace(/^data:image\/\w+;base64,/, ""),'base64')
+  const buffer = new Buffer(data.replace(/^data:image\/\w+;base64,/, ''),'base64');
   const bucket = config.get('AWS_BUCKET');
-  const key = `${uuid()}.png`;
-  var data = {
+  const key = `${uuid()}.${extension}`;
+  const payload = {
     Key: key, 
     Body: buffer,
     ContentEncoding: 'base64',
@@ -22,11 +21,11 @@ const upload = (data)=> {
     ACL: 'public-read',
     Bucket: bucket
   };
-  return S3.putObject(data)
+  return S3.putObject(payload)
     .promise()
     .then(()=> {
       return `https://s3.amazonaws.com/${bucket}/${key}`;
-    })
+    });
 };
 
 module.exports = {
